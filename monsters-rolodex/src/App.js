910fe9e19d59e357087f078af5889d98b2499b78
Sanjay Das <sanjayda15@gmail.com>
2021-04-20 22:46:59 +0530

    first commit

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return (
+      <div className="App">
+        <input type='search' placeholder='search monsters' onChange={e =>
+          this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
+
+      </div>
+    )
+  }
+}
+export default App;
